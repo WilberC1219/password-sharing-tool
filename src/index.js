8166a90413141bf8bcdbcf4db2cfe066c46809fc
WilberC1219 <wilberclaudio@gmail.com>
2023-06-13 19:57:09 -0400

    changed port for server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 // create server and start listening
 const app = express();
-const port = 5500;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
