74a9c6ef49a572e984c20d7a9249987d2950eb11
WilberC1219 <wilberclaudio@gmail.com>
2023-06-20 16:11:11 -0400

    added code to retrieve users from database

--- src/db_connect.js
+++ src/db_connect.js
@@ -4,6 +4,7 @@ const user = require("./models/user");
 const User = user();
 (async () => {
   try {
+    /* Code to create and store a user into the database
     const usr = await User.create({
       firstName: "wilber",
       lastName: "Claudio",
@@ -11,7 +12,18 @@ const User = user();
       email: "example2@example2.com",
     });
     console.log(usr);
+    */
+
+    /* Code to retrieve user from database (if they exist) */
+    const usr = await User.findOne({ where: { email: "seed@seedemail.com" } });
+    if (usr === null) {
+      console.log("Not found!");
+    } else {
+      console.log(usr instanceof User); // true
+      console.log(usr); // usr instance
+    }
   } catch (error) {
+    //error.name is the sql error, error.errors[0].message describe cause of error
     console.error(`${error.name}, ${error.errors[0].message}`);
   }
 })();
