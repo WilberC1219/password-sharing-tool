81a8af19e3af3b310b9f823a312519154b7474ba
WilberC1219 <wilberclaudio@gmail.com>
2023-06-21 17:40:21 -0400

    added url path signup

--- src/server.js
+++ src/server.js
@@ -10,3 +10,18 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.post("/signup", async (req, res) => {
+  try {
+    //example code to sign up a user
+    /*const created = await new users(db).createUser(req.body);
+    if (!created) {
+      return res.status(400).json({ message: "Sign up failed" });
+    }
+    */
+    res.json({ message: "done", status: 200 });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "internal_server_error" });
+  }
+});
