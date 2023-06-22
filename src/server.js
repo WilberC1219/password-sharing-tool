28a7df3406fb0034f831d10afd5609cc74e1d25d
WilberC1219 <wilberclaudio@gmail.com>
2023-06-22 18:31:36 -0400

    Completed /signup post request handling

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const { uuidWithPrefix } = require("./utils/uuid");
 const env = process.env.NODE_ENV || "development";
 const express = require("express");
 const config = require(__dirname + "/config/database.json")[env];
@@ -19,24 +18,15 @@ app.get("/", (req, res) => {
 
 app.post("/signup", async (req, res) => {
   try {
-    //example code to sign up a user
+    const creationResult = await User.createUser(req.body);
 
-    /*const created = await new users(db).createUser(req.body);
-    if (!created) {
-      return res.status(400).json({ message: "Sign up failed" });
+    if (creationResult.errors) {
+      res.status(400).json({ message: `Sign up failed` });
+    } else {
+      res.status(200).json({ message: "User sign up successful" });
     }
-    */
-    console.log(req.body);
-    // create the user instance, the create object will store it into the database
-    const newUser = await User.create(req.body);
-    res.json({ message: "User sign up successful", status: 200 });
   } catch (e) {
     console.error(e);
-
-    if (e.errors[0].instance instanceof User) {
-      res.status(400).json({ message: "Sign up failed" });
-    } else {
-      res.status(500).json({ message: "internal_server_error" });
-    }
+    res.status(500).json({ message: "internal_server_error" });
   }
 });
