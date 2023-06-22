cdbc8fbbfc71f0416e25b0cd90a2d3fea7c335ce
WilberC1219 <wilberclaudio@gmail.com>
2023-06-22 16:52:53 -0400

    added more code to test /signup post req

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const { uuidWithPrefix } = require("./utils/uuid");
+const env = process.env.NODE_ENV || "development";
 const express = require("express");
-
+const config = require(__dirname + "/config/database.json")[env];
+const User = require("./models/user")(new Sequelize(config), DataTypes);
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
@@ -14,14 +20,23 @@ app.get("/", (req, res) => {
 app.post("/signup", async (req, res) => {
   try {
     //example code to sign up a user
+
     /*const created = await new users(db).createUser(req.body);
     if (!created) {
       return res.status(400).json({ message: "Sign up failed" });
     }
     */
-    res.json({ message: "done", status: 200 });
+    console.log(req.body);
+    // create the user instance, the create object will store it into the database
+    const newUser = await User.create(req.body);
+    res.json({ message: "User sign up successful", status: 200 });
   } catch (e) {
     console.error(e);
-    res.status(500).json({ message: "internal_server_error" });
+
+    if (e.errors[0].instance instanceof User) {
+      res.status(400).json({ message: "Sign up failed" });
+    } else {
+      res.status(500).json({ message: "internal_server_error" });
+    }
   }
 });
