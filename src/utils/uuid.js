62c0310f447e9a33b3dfb394633f69920e5c8da6
WilberC1219 <wilberclaudio@gmail.com>
2023-06-23 23:26:40 -0400

    documented both uuid and uuidWithPrefix

--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -1,5 +1,10 @@
 const UUID = require("uuid");
 
+/**
+ * Generates a UUID using the v4 version of the UUID algorithm.
+ * @param {boolean} [removeDashes=false] - Flag indicating whether to remove dashes from the generated UUID.
+ * @returns {string} Generated UUID.
+ */
 const uuid = (removeDashes = false) => {
   if (removeDashes) {
     return UUID.v4().replace(/-/g, "");
@@ -8,6 +13,12 @@ const uuid = (removeDashes = false) => {
   return UUID.v4();
 };
 
+/**
+ * Generates a UUID with a prefix using the v4 version of the UUID algorithm.
+ * @param {boolean} [removeDashes=false] - Flag indicating whether to remove dashes from the generated UUID.
+ * @param {string} prefix - Prefix to add to the generated UUID.
+ * @returns {string} Generated UUID with the specified prefix.
+ */
 const uuidWithPrefix = (removeDashes = false, prefix) => `${prefix}-${uuid(removeDashes)}`;
 
 module.exports = {
