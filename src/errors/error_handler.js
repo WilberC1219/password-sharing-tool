4adbaff11ebe1a58e01302c73617ebcef1cd4108
WilberC1219 <wilberclaudio@gmail.com>
2023-06-25 16:01:06 -0400

    renamed error_utils.js to error_handler.js

---
+++ src/errors/error_handler.js
@@ -0,0 +1,28 @@
+const { InternalError } = require("./errors");
+/**
+ * Generates a standardized error response data on the provided error object.
+ * @param {Error} error - The error object.
+ * @param {message} message - The error message
+ * @returns {Object} An object with statusCode, errorMessage, and cause attributes.
+ */
+function getErrorResponse(error, message) {
+  const statusCode = error.code ? error.code : 400;
+
+  // an error defined by sequelize
+  if (error.errors && error.errors.length > 0) {
+    const errorMap = {};
+
+    error.errors.map((e) => {
+      if (e.type.toLowerCase() === "unique violation" && e.path === "id") {
+        statusCode = 500;
+      }
+      errorMap[e.type] = (errorMap[e.type] || []).concat({ field: e.path, validator: e.validatorKey });
+    });
+    return { statusCode, errorMessage: message, errorDetails: errorMap };
+  }
+
+  // anything other error is an error from the errors directory
+  return { statusCode, errorMessage: message, errorDetails: { type: error.name, cause: error.message } };
+}
+
+module.exports = { getErrorResponse };
