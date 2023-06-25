66fc0bbaed327c01a6da9ba08f4c989cdb791d16
WilberC1219 <wilberclaudio@gmail.com>
2023-06-25 18:37:32 -0400

    added genjwt.js in utils directory. generates jwt

---
+++ src/utils/genjwt.js
@@ -0,0 +1,18 @@
+const { InternalError } = require("../errors/errors");
+const { sign } = require("jsonwebtoken");
+/**
+ * creates a json web token string with the provided payload.
+ * @param {Object} payload - The payload being turned into a json web token string.
+ * @returns {string} - A json web token string.
+ * @throws {InternalError} If an unexpected error occurs while generating a json web token string
+ */
+async function genJwt(payload) {
+  try {
+    const jwt = await sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+    return jwt;
+  } catch (error) {
+    throw InternalError();
+  }
+}
+
+module.exports = { genJwt };
